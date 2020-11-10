fadab9430bcca8b88639b5a2df2bf3fbd44c01f4
Gabriel Ruivo <gabriel-ruivo@hotmail.com>
2020-11-09 21:16:54 -0300

    challenge01

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Challenge01 from './Challenge01/challenge01';
+
+ReactDOM.render(<Challenge01 />, document.getElementById('root'));
